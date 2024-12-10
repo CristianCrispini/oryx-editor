8a9e5f95739026eba05f43d44fd521941b80bb7b
Cristian Crispini <cristian.crispini@dos.design>
2024-12-10 16:25:49 +0100

    procedural languages created by dockerfile.

--- poem-jvm/data/database/db_schema.sql
+++ poem-jvm/data/database/db_schema.sql
@@ -8,18 +8,16 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
 
+-- Procedural languages Created By Dockerfile
 --
 -- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: poem
 --
-
-
+ALTER PROCEDURAL LANGUAGE plpgsql OWNER TO poem;
 --
 -- Name: plpythonu; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: poem
 --
 
-CREATE PROCEDURAL LANGUAGE plpythonu;
 ALTER PROCEDURAL LANGUAGE plpythonu OWNER TO poem;
-
 SET search_path = public, pg_catalog;
 
 --
