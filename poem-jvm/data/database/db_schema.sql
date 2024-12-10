2b9e8b41751c39ba1bdbdfa62978897ab0851b4a
Cristian Crispini <cristian.crispini@dos.design>
2024-12-10 11:20:20 +0100

    The error message "language "plpgsql" already exists" indicates that you're trying to create the plpgsql procedural language in a PostgreSQL database where it's already installed. plpgsql is typically installed by default in PostgreSQL.

--- poem-jvm/data/database/db_schema.sql
+++ poem-jvm/data/database/db_schema.sql
@@ -12,10 +12,6 @@ SET escape_string_warning = off;
 -- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: poem
 --
 
-CREATE PROCEDURAL LANGUAGE plpgsql;
-
-
-ALTER PROCEDURAL LANGUAGE plpgsql OWNER TO poem;
 
 --
 -- Name: plpythonu; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: poem
